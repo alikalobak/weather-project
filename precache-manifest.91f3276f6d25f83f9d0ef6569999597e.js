c364c09ddc1bc25946a0aaa3f23b34e62c2ac3c3
alikalobak <alikalobak@gmail.com>
2020-03-26 16:32:53 +0200

    Updates

---
+++ precache-manifest.91f3276f6d25f83f9d0ef6569999597e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2977d4ced1ebc7b011476b8b36ff5ec6",
+    "url": "/weather-project/index.html"
+  },
+  {
+    "revision": "519f99138a0477ccdf8a",
+    "url": "/weather-project/static/js/2.a89c5502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-project/static/js/2.a89c5502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51629dc79c8e750861f",
+    "url": "/weather-project/static/js/main.463a4040.chunk.js"
+  },
+  {
+    "revision": "816743d35dca0a66147b",
+    "url": "/weather-project/static/js/runtime-main.6eacfc1d.js"
+  },
+  {
+    "revision": "226bf20b0756044e92cb3717ab23e9a6",
+    "url": "/weather-project/static/media/clear.226bf20b.svg"
+  }
+]);
